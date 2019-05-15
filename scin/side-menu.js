2a52798073d976a21184154e905a07d27704ea8f
geneticacricetusa <50636619+geneticacricetusa@users.noreply.github.com>
2019-05-15 20:48:34 +0500

    Добавлены папки с вложенными файлами.

---
+++ scin/side-menu.js
@@ -0,0 +1,10 @@
+function menu_swapClass(){ 
+ var i,x,tB,j=0,tA=new Array(),arg=menu_swapClass.arguments;
+if(document.getElementsByTagName){for(i=4;i<arg.length;i++){tB=document.getElementsByTagName(arg[i]);
+for(x=0;x<tB.length;x++){tA[j]=tB[x];j++;}}for(i=0;i<tA.length;i++){
+if(tA[i].className){if(tA[i].id==arg[1]){if(arg[0]==1){
+tA[i].className=(tA[i].className==arg[3])?arg[2]:arg[3];}else{tA[i].className=arg[2];}
+}else if(arg[0]==1 && arg[1]=='none'){if(tA[i].className==arg[2] || tA[i].className==arg[3]){
+tA[i].className=(tA[i].className==arg[3])?arg[2]:arg[3];}
+}else if(tA[i].className==arg[2]){tA[i].className=arg[3];}}}}
+}
